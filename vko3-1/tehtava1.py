5eeebc7f565c96ff7951a367b2a2ee381c9cb672
Anna <annamari.vanhanen@gmail.com>
2021-10-29 13:07:22 +0300

    viimeinen versio

--- vko3-1/tehtava1.py
+++ vko3-1/tehtava1.py
@@ -7,31 +7,13 @@ sr = requests.get(' https://2ri98gd9i4.execute-api.us-east-1.amazonaws.com/dev/a
 data = sr.json()
 
 parametrilista = []
+
 for d in data['items']:
-    parametrilista.append({d["parameter"]})
-print(parametrilista)
-string = " "
-string = string.join(parametrilista)
-string.split(",")
+    parametrilista.append(d["parameter"])
+
 with open("checkpoint.txt", "w") as tiedosto:
-    for i in string:
+    for i in parametrilista:
         tiedosto.write(i + "\n")
-
-
-def luo_bucket(bucket_nimi :str):
-
-    credentials = service_account.Credentials.from_service_account_file("C:/Users/35840/.vscode/python_harkat/uusiprojektivko2-keys.json")
-    valimies = storage.Client(credentials=credentials)
-
-    # Luo uuden bucketin
-    bucket = valimies.create_bucket(bucket_nimi)
-
-    print("Loi bucketin {} .".format(bucket.name))
-
-if __name__ == "__main__":
-
-    luo_bucket("checkpointvko3")
-
 
 def tiedosto_buckettiin(bucket_name, source_file_name, destination_blob_name):
 
@@ -53,3 +35,19 @@ def tiedosto_buckettiin(bucket_name, source_file_name, destination_blob_name):
 
 if __name__ == "__main__":
     tiedosto_buckettiin("checkpointvko3",r"C:\Users\35840\vscode\Git\vko3-1\checkpoint.txt", "Checkpoint1")
+
+
+def luo_bucket(bucket_nimi :str):
+
+    credentials = service_account.Credentials.from_service_account_file("C:/Users/35840/.vscode/python_harkat/uusiprojektivko2-keys.json")
+    valimies = storage.Client(credentials=credentials)
+
+    # Luo uuden bucketin
+    bucket = valimies.create_bucket(bucket_nimi)
+
+    print("Loi bucketin {} .".format(bucket.name))
+
+if __name__ == "__main__":
+
+    luo_bucket("checkpointvko3")
+
